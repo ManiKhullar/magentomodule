a78ed3055c50f53c36fc401ba1c4d88f30b43400
ManiKhullar <kmanidev6@gmail.com>
2022-02-21 08:33:27 -0500

    new push

--- module-support/Console/OrderExportFromIds.php
+++ module-support/Console/OrderExportFromIds.php
@@ -2,7 +2,7 @@
 /**
  * OrderExportFromIds.php
  * @package   Altayer\Support\Console
- * @author    Amrendra <amrendragr8@gmail.com>
+ * @author    Mani <kmanidev6@gmail.com>
  */
 
 namespace Altayer\Support\Console;
